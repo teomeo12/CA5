458a563c2ab0b26dc5cc41fd6dcea4a2c797dc91
teomeo12 <d00240834@student.dkit.ie>
2022-02-25 12:08:30 +0000

    First Commit. Add Name and Group

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Name: Teodor Donchev SD2a
  * Hello world!
@@ -9,8 +12,23 @@ public class App
 {
     public static void main( String[] args )
     {
+        App app = new App();
+        app.start();
+
+    }
+    public void start(){
         System.out.println( "Hello World!" );
         System.out.println("Something  fd");
+        ArrayList<String> nameList =new ArrayList<>();
+        initialize(nameList);
+
+    }
+    private void initialize(List list){
+        list.add(new String("Ben"));
+        list.add(new String("Jerry"));
+
+    }
+    public void display(){
 
     }
 }
