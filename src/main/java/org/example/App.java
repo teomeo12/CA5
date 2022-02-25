7e8429911c24bca8afb2e0d3b94e63cb34c56f37
teomeo12 <d00240834@student.dkit.ie>
2022-02-25 11:46:41 +0000

    First Commit. Add Name and Group

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Name: Teodor Donchev SD2a
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
