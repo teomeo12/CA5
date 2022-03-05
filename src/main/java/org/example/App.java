88664e2577735621b256c40f04804365bf76962d
teomeo12 <d00240834@student.dkit.ie>
2022-03-05 21:45:22 +0000

    Instantiate an ArrayList<Singer> of Singers. Singer Class declared. Display method to display all the records from the arraylist.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
 
 /**
  * Name: Teodor Donchev SD2a
@@ -17,18 +18,34 @@ public class App
 
     }
     public void start(){
-        System.out.println( "Hello World!" );
-        System.out.println("Something  fd");
-        ArrayList<String> nameList =new ArrayList<>();
-        initialize(nameList);
+        //Singer singer1 = new Singer();
+        ArrayList<Singer> singersList = new ArrayList<>();
+        initialize(singersList);
+        display(singersList);
 
     }
     private void initialize(List list){
-        list.add(new String("Ben"));
-        list.add(new String("Jerry"));
+        list.add(new Singer("Mark Lanegan", LocalDate.parse("2007-12-03"),2000.00,"rock"));
+        list.add(new Singer("Kylie Minogue",LocalDate.parse("2007-12-03"),3000.00,"pop"));
+        list.add(new Singer("Declan Nerney",LocalDate.parse("2007-12-03"),4000.00,"country"));
+        list.add(new Singer("Alice in Chains ",LocalDate.parse("2007-12-03"),30000.00,"Grunge"));
+        list.add(new Singer("Mattiel",LocalDate.parse("2000-12-04"),14000.00,"pop"));
+        list.add(new Singer("King of Leon",LocalDate.parse("1999-12-05"),34000.00,"rock"));
+        list.add(new Singer("Royal Blood",LocalDate.parse("2007-12-03"),25000.00,"metal"));
+        list.add(new Singer("Last Hope",LocalDate.parse("2007-12-03"),5000.00,"hard-core"));
+        list.add(new Singer("Odd Crew",LocalDate.parse("2007-12-03"),10000.00,"metal"));
+        list.add(new Singer("PJ Harvey",LocalDate.parse("2007-12-03"),20000.00,"pop-rock"));
 
     }
-    public void display(){
+    public void display(List <Singer> singersList){
+       // ,"Date of Birth","Rate","Genre"
+        System.out.printf("%-20s %-20s %-10s %-20s","Name","Date of Birth","Rate","Genre");
+        System.out.println("\n---------------------------------------------------------------");
+        for (Singer singer : singersList ) {
 
+            System.out.printf("\n%-20s %-20s %-10s %-20s",singer.getName(),singer.getDob(), singer.getRate(),singer.getGenre() );
+          //  System.out.println(singer.getDob());
+        }
+        System.out.println("\n---------------------------------------------------------------");
     }
 }
