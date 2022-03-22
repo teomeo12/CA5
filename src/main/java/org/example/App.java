60cf49b3cefff965114f26cb0b6c22998a481b6e
teomeo12 <d00240834@student.dkit.ie>
2022-03-22 13:23:03 +0000

    Priority queue with rate priority SortType class created. ComparatorSingerRate comparator class created.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,8 @@ package org.example;
 import java.time.LocalTime;
 import java.util.*;
 import java.time.LocalDate;
+import java.util.PriorityQueue;
+import java.util.Comparator;
 
 /**
  * Name: Teodor Donchev SD2a
@@ -16,31 +18,29 @@ public class App
         App app = new App();
         app.start();
 
-
     }
     public void start() {
         treeMap();
         hashmap();
-
+        singerRateComparator();
     }
         public static void hashmap()  // TREE Map (Student => Book )
         {
         ArrayList<Singer> singersList = new ArrayList<>();
         ArrayList<Venue> venuetList = new ArrayList<>();
 
-        display(singersList);
 
 
-        Singer s1 = new Singer(1,"Mark Lanegan", LocalDate.parse("2007-12-03"),2000.00,"rock");
-        Singer s2 =(new Singer(2,"Kylie Minogue",LocalDate.parse("2007-12-03"),3000.00,"pop"));
-        Singer s3 =(new Singer(3,"Declan Nerney",LocalDate.parse("2007-12-03"),4000.00,"country"));
-        Singer s4 =(new Singer(4,"Alice in Chains ",LocalDate.parse("2007-12-03"),30000.00,"Grunge"));
-        Singer s5 =(new Singer(5,"Mattiel",LocalDate.parse("2000-12-04"),14000.00,"pop"));
-        Singer s6 =(new Singer(6,"King of Leon",LocalDate.parse("1999-12-05"),34000.00,"rock"));
-        Singer s7 =(new Singer(7,"Royal Blood",LocalDate.parse("2007-12-03"),25000.00,"metal"));
-        Singer s8 =(new Singer(8,"Last Hope",LocalDate.parse("2007-12-03"),5000.00,"hard-core"));
-        Singer s9 =(new Singer(9,"Odd Crew",LocalDate.parse("2007-12-03"),10000.00,"metal"));
-        Singer s10 =(new Singer(10,"PJ Harvey",LocalDate.parse("2007-12-03"),20000.00,"pop-rock"));
+        Singer s1 = new Singer(1,"Mark Lanegan", LocalDate.parse("1954-12-03"),2000.00,"Rock");
+        Singer s2 =(new Singer(2,"Kylie Minogue",LocalDate.parse("1965-12-03"),3000.00,"Pop"));
+        Singer s3 =(new Singer(3,"Declan Nerney",LocalDate.parse("1975-12-03"),4000.00,"Country"));
+        Singer s4 =(new Singer(4,"Alice in Chains ",LocalDate.parse("1993-12-03"),30000.00,"Grunge"));
+        Singer s5 =(new Singer(5,"Mattiel",LocalDate.parse("2000-12-04"),14000.00,"Pop"));
+        Singer s6 =(new Singer(6,"King of Leon",LocalDate.parse("1999-12-05"),34000.00,"Rock"));
+        Singer s7 =(new Singer(7,"Royal Blood",LocalDate.parse("1983-12-03"),25000.00,"Metal"));
+        Singer s8 =(new Singer(8,"Last Hope",LocalDate.parse("1986-12-03"),5000.00,"Hard-core"));
+        Singer s9 =(new Singer(9,"Odd Crew",LocalDate.parse("1993-12-03"),10000.00,"Metal"));
+        Singer s10 =(new Singer(10,"PJ Harvey",LocalDate.parse("1974-12-03"),20000.00,"Pop-rock"));
 
         singersList.add(s1);
         singersList.add(s2);
@@ -52,6 +52,8 @@ public class App
         singersList.add(s8);
         singersList.add(s9);
         singersList.add(s10);
+
+            display(singersList);
 
         Venue v1 = new Venue(1,"O2 arena","Dublin", LocalTime.parse("22:30:00"));
         Venue v2 =new Venue(2,"Live Music Hall","Germany", LocalTime.parse("20:00:00"));
@@ -88,8 +90,9 @@ public class App
             singerVenue.put(s9,v9);
             singerVenue.put(s10,v10);
 
-        System.out.println("Map: [ Singer -> Venue ]");
+        System.out.println("Hash map: [ Singer -> Venue ]");
         // for each Entry in the set of all entries
+            System.out.println("=============================================");
         for (Map.Entry<Singer, Venue> entry : singerVenue.entrySet()) {
             Singer singer = entry.getKey();
             Venue venue = entry.getValue();
@@ -100,7 +103,8 @@ public class App
     }
     public static void display(List <Singer> singersList){
         // ,"Date of Birth","Rate","Genre"
-        System.out.printf("%-5s %-20s %-20s %-10s %-20s","id","Name","Date of Birth","Rate","Genre");
+        System.out.println("\n---------------------------------------------------------------");
+        System.out.printf("%-5s %-20s %-20s %-10s %-20s","Id","Name","Date of Birth","Rate","Genre");
         System.out.println("\n---------------------------------------------------------------");
         for (Singer singer : singersList ) {
 
@@ -112,20 +116,21 @@ public class App
 
     public static void treeMap()  // TREE Map (Student => Book )
     {
-        System.out.println("Tree map");
+
         // Student class is used as the Key in this TreeMap,
         // so, Student must implement Comparable or
         // a Comparator must be supplied to the Map on construction
-        Singer s1 = new Singer(1,"Mark Lanegan", LocalDate.parse("2007-12-03"),2000.00,"rock");
-        Singer s2 =(new Singer(2,"Kylie Minogue",LocalDate.parse("2007-12-03"),3000.00,"pop"));
-        Singer s3 =(new Singer(3,"Declan Nerney",LocalDate.parse("2007-12-03"),4000.00,"country"));
-        Singer s4 =(new Singer(4,"Alice in Chains ",LocalDate.parse("2007-12-03"),30000.00,"Grunge"));
-        Singer s5 =(new Singer(5,"Mattiel",LocalDate.parse("2000-12-04"),14000.00,"pop"));
-        Singer s6 =(new Singer(6,"King of Leon",LocalDate.parse("1999-12-05"),34000.00,"rock"));
-        Singer s7 =(new Singer(7,"Royal Blood",LocalDate.parse("2007-12-03"),25000.00,"metal"));
-        Singer s8 =(new Singer(8,"Last Hope",LocalDate.parse("2007-12-03"),5000.00,"hard-core"));
-        Singer s9 =(new Singer(9,"Odd Crew",LocalDate.parse("2007-12-03"),10000.00,"metal"));
-        Singer s10 =(new Singer(10,"PJ Harvey",LocalDate.parse("2007-12-03"),20000.00,"pop-rock"));
+        Singer s1 = new Singer(1,"Mark Lanegan", LocalDate.parse("1954-12-03"),2000.00,"Rock");
+        Singer s2 =(new Singer(2,"Kylie Minogue",LocalDate.parse("1965-12-03"),3000.00,"Pop"));
+        Singer s3 =(new Singer(3,"Declan Nerney",LocalDate.parse("1975-12-03"),4000.00,"Country"));
+        Singer s4 =(new Singer(4,"Alice in Chains ",LocalDate.parse("1993-12-03"),30000.00,"Grunge"));
+        Singer s5 =(new Singer(5,"Mattiel",LocalDate.parse("2000-12-04"),14000.00,"Pop"));
+        Singer s6 =(new Singer(6,"King of Leon",LocalDate.parse("1999-12-05"),34000.00,"Rock"));
+        Singer s7 =(new Singer(7,"Royal Blood",LocalDate.parse("1983-12-03"),25000.00,"Metal"));
+        Singer s8 =(new Singer(8,"Last Hope",LocalDate.parse("1986-12-03"),5000.00,"Hard-core"));
+        Singer s9 =(new Singer(9,"Odd Crew",LocalDate.parse("1993-12-03"),10000.00,"Metal"));
+        Singer s10 =(new Singer(10,"PJ Harvey",LocalDate.parse("1974-12-03"),20000.00,"Pop-rock"));
+
 
         Venue v1 = new Venue(1,"O2 arena","Dublin", LocalTime.parse("22:30:00"));
         Venue v2 =new Venue(2,"Live Music Hall","Germany", LocalTime.parse("20:00:00"));
@@ -150,7 +155,58 @@ public class App
         singerVenue.put(s9,v9);
         singerVenue.put(s10,v10);
 
+
+        System.out.println("\nTree Map: [ Singer -> Venue ]");
+        // for each Entry in the set of all entries
+        System.out.println("=============================================");
+        for (Map.Entry<Singer, Venue> entry : singerVenue.entrySet()) {
+            Singer singer = entry.getKey();
+            Venue venue = entry.getValue();
+            System.out.println("Singer: " + singer.getName() + ", sings at " + venue.getLocation() +" ,time: "+ venue.getTime());
+        }
+        System.out.println("=============================================");
+
     }
+
+    public static void singerRateComparator(){
+
+        Singer s1 = new Singer(1,"Mark Lanegan", LocalDate.parse("1954-12-03"),2000.00,"Rock");
+        Singer s2 =(new Singer(2,"Kylie Minogue",LocalDate.parse("1965-12-03"),3000.00,"Pop"));
+        Singer s3 =(new Singer(3,"Declan Nerney",LocalDate.parse("1975-12-03"),4000.00,"Country"));
+        Singer s4 =(new Singer(4,"Alice in Chains ",LocalDate.parse("1993-12-03"),30000.00,"Grunge"));
+        Singer s5 =(new Singer(5,"Mattiel",LocalDate.parse("2000-12-04"),14000.00,"Pop"));
+        Singer s6 =(new Singer(6,"King of Leon",LocalDate.parse("1999-12-05"),34000.00,"Rock"));
+        Singer s7 =(new Singer(7,"Royal Blood",LocalDate.parse("1983-12-03"),25000.00,"Metal"));
+        Singer s8 =(new Singer(8,"Last Hope",LocalDate.parse("1986-12-03"),5000.00,"Hard-core"));
+        Singer s9 =(new Singer(9,"Odd Crew",LocalDate.parse("1993-12-03"),10000.00,"Metal"));
+        Singer s10 =(new Singer(10,"PJ Harvey",LocalDate.parse("1974-12-03"),20000.00,"Pop-rock"));
+
+
+        PriorityQueue<Singer> queue = new PriorityQueue<>(new ComparatorSingerRate(SortType.Ascending));
+
+        queue.add(s1);
+        queue.add(s2);
+        queue.add(s3);
+        queue.add(s4);
+        queue.add(s5);
+        queue.add(s6);
+        queue.add(s7);
+        queue.add(s8);
+        queue.add(s9);
+        queue.add(s10);
+
+
+        System.out.println("\nValues in order of Rate Priority:");
+        // remove
+        System.out.println("=============================================");
+        while (!queue.isEmpty()) {
+            System.out.println(queue.remove());
+        }
+        System.out.println("=============================================");
+
+    }
+
+
 
 
 }
